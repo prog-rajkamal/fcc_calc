6923597383b526e595fc887f292c32219e2e46e4
Raj Kamal <rajkamal6393@gmail.com>
2017-10-19 10:27:59 +0530

    Refactor: buttonTypes are now object properrites

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
     constructor() {
         super();
         this.buttonPressed.bind(this);
-        const buttonTypes = {
+        this.buttonTypes = {
             Command: "Command",
             Input: "Input",
             Operator: "Operator"
@@ -14,97 +14,97 @@ class App extends Component {
         this.buttons = [
             {
                 display: "AC",
-                type: buttonTypes.Command,
+                type: this.buttonTypes.Command,
                 order: 1
             },
             {
                 display: "C",
-                type: buttonTypes.Command,
+                type: this.buttonTypes.Command,
                 order: 30
             },
             {
-                display: "÷",
-                type: buttonTypes.Operator,
+                display: " ÷ ",
+                type: this.buttonTypes.Operator,
                 order: 70
             },
             {
-                display: "×",
-                type: buttonTypes.Operator,
+                display: " × ",
+                type: this.buttonTypes.Operator,
                 order: 85
             },
             {
-                display: "-",
-                type: buttonTypes.Operator,
+                display: " - ",
+                type: this.buttonTypes.Operator,
                 order: 90
             },
             {
-                display: "+",
-                type: buttonTypes.Operator,
+                display: " + ",
+                type: this.buttonTypes.Operator,
                 order: 95
             },
             {
                 display: "=",
-                type: buttonTypes.Operator,
+                type: this.buttonTypes.Operator,
                 order: 100
             },
             {
-                display: "%",
-                type: buttonTypes.Operator,
+                display: " MOD ",
+                type: this.buttonTypes.Operator,
                 order: 10
             },
             {
                 display: ".",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 75
             },
             {
                 display: "9",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 71
             },
             {
                 display: "8",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 32
             },
             {
                 display: "7",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 5
             },
             {
                 display: "6",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 72
             },
             {
                 display: "5",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 33
             },
             {
                 display: "4",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 6
             },
             {
                 display: "3",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 73
             },
             {
                 display: "2",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 34
             },
             {
                 display: "1",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 7
             },
             {
                 display: "0",
-                type: buttonTypes.Input,
+                type: this.buttonTypes.Input,
                 order: 35
             }
             //    'AC' , '7', '4', '1' , '%',
@@ -112,8 +112,13 @@ class App extends Component {
             // '÷' , '9', '6', '3' , '.',
             // '×' , '-', '+', '='
         ];
-        console.log(this.buttons);
-        this.buttons = this.buttons.sort((btna, btnb) => btna.order > btnb.order);
+        this.buttons = this.buttons.sort(
+            (btna, btnb) => btna.order > btnb.order
+        );
+        this.state = {
+            calc_result: "",
+            calc_operation: ""
+        };
     }
     render() {
         return (
@@ -125,23 +130,90 @@ class App extends Component {
     }
 
     renderCalculator() {
-
         return (
             <div className="calculator">
-                <div class="calculator__output">
-                    <p class="calc__result">134.5</p>
-                    <p class="calc__expression">8 &times; 5 / 7</p>
+                <div className="calculator__output">
+                    <div className="calc__result">{this.state.calc_result}</div>
+                    <div className="calc__expression">
+                        {this.state.calc_operation}
+                    </div>
                 </div>
                 <div className="calculator__input">
                     {this.buttons.map(btn => (
-                        <button class="calc__button">{btn.display}</button>
+                        <button
+                            className="calc__button"
+                            key={btn.order}
+                            onClick={e => this.buttonPressed(e, btn)}
+                        >
+                            {btn.display}
+                        </button>
                     ))}
                 </div>
             </div>
         );
     }
 
-    buttonPressed(btn) {}
+    buttonPressed(e, btn) {
+        // console.log(btn);
+        e.preventDefault();
+        if (btn.type === this.buttonTypes.Command) {
+            if (btn.display === "AC") {
+                this.setState({
+                    calc_operation: "",
+                    calc_result: ""
+                });
+            } else if (btn.display === "C") {
+                this.setState(state => {
+                    return {
+                        calc_operation: state.calc_operation.slice(
+                            0,
+                            state.calc_operation.length - 1
+                        )
+                    };
+                });
+            }
+            return;
+        }
+        if (btn.type === this.buttonTypes.Input) {
+            this.setState(state => {
+                return {
+                    calc_operation: state.calc_operation + btn.display
+                };
+            });
+            return;
+        }
+
+        if (btn.type === this.buttonTypes.Operator && btn.display === "=") {
+            this.setState(state => {
+                return {
+                    calc_result: this.calculate()
+                };
+            });
+            return;
+        } else {
+            this.setState(state => {
+                return {
+                    calc_operation: state.calc_operation + btn.display
+                };
+            });
+            return;
+        }
+    }
+
+    calculate() {
+        let res = 0;
+        let ops = this.state.calc_operation;
+        try {
+            // debugger;
+            ops = ops.replace(/÷/g, "/");
+            ops = ops.replace(/×/g, "*");
+            ops = ops.replace(/MOD/g, "%");
+            res = eval(ops);
+        } catch (error) {
+            res = "Error!";
+        }
+        return res;
+    }
 }
 
 export default App;
